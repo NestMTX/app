e2117cd89e1fad83a1685ef588341777beed03e9
Jak Giveon <jak@feriority.com>
2024-09-10 19:29:55 -0400

    WIP

--- app/services/streamer.ts
+++ app/services/streamer.ts
@@ -41,18 +41,12 @@ export class StreamerService {
   #ffmpegHwAccelerator?: string
   #ffmpegHwAcceleratorDevice?: string
 
-  #noSuchCameraPort?: number
-  #cameraDisabledPort?: number
-  #connectingPort?: number
   #internalApiServer?: StreamPrivateApiServer
 
   constructor(app: ApplicationService) {
     this.#app = app
     this.#managedProcesses = new Set()
     this.#shuttingDownProcesses = new Set()
-    // this.#noSuchCameraPort = env.get('NO_SUCH_CAMERA_PORT', 62001)
-    // this.#cameraDisabledPort = env.get('CAMERA_DISABLED_PORT', 62002)
-    // this.#connectingPort = env.get('CONNECTING_PORT', 62003)
     this.#internalApiPort = env.get('INTERNAL_API_PORT', 62005)
   }
 
@@ -114,52 +108,12 @@ export class StreamerService {
     pm3.on('log:out', this.#logProcessToInfo)
     pm3.on('log:err', this.#logProcessToWarn)
 
-    const noSuchCameraFilePath = this.#app.makePath('resources/mediamtx/no-such-camera.jpg')
-    const cameraDisabledFilePath = this.#app.makePath('resources/mediamtx/camera-disabled.jpg')
-    const connectingFilePath = this.#app.makePath('resources/mediamtx/connecting.jpg')
-    this.#noSuchCameraPort = await pickPort({ type: 'tcp', ip: '127.0.0.1' })
-    this.#cameraDisabledPort = await pickPort({ type: 'tcp', ip: '127.0.0.1' })
-    this.#connectingPort = await pickPort({ type: 'tcp', ip: '127.0.0.1' })
-
     pm3.on('stdout:nestmtx-static-no-such-camera', this.#logToInfo)
     pm3.on('stderr:nestmtx-static-no-such-camera', this.#logToError)
     pm3.on('stdout:nestmtx-static-camera-disabled', this.#logToInfo)
     pm3.on('stderr:nestmtx-static-camera-disabled', this.#logToError)
     pm3.on('stdout:nestmtx-static-connecting', this.#logToInfo)
     pm3.on('stderr:nestmtx-static-connecting', this.#logToError)
-    pm3.add(
-      'nestmtx-static-no-such-camera',
-      {
-        file: 'node',
-        arguments: ['ace', 'mjpeg:stream', this.#noSuchCameraPort.toString(), noSuchCameraFilePath],
-        restart: true,
-      },
-      true
-    )
-    pm3.add(
-      'nestmtx-static-camera-disabled',
-      {
-        file: 'node',
-        arguments: [
-          'ace',
-          'mjpeg:stream',
-          this.#cameraDisabledPort.toString(),
-          cameraDisabledFilePath,
-        ],
-        restart: true,
-      },
-      true
-    )
-    pm3.add(
-      'nestmtx-static-connecting',
-      {
-        file: 'node',
-        arguments: ['ace', 'mjpeg:stream', this.#connectingPort.toString(), connectingFilePath],
-        restart: true,
-      },
-      true
-    )
-    // this.#internalApiPort = await pickPort({ type: 'tcp', ip: '127.0.0.1' })
     this.#internalApiServer = new StreamPrivateApiServer({
       serveClient: false,
       allowEIO3: true,
@@ -167,20 +121,13 @@ export class StreamerService {
     })
     this.#internalApiServer.on('connection', (socket) => {
       this.#logger?.info(`Got connection from socket ${socket.id}`)
-      if (this.#noSuchCameraPort && this.#cameraDisabledPort && this.#connectingPort) {
-        socket.emit('ports', {
-          cameraMissing: this.#noSuchCameraPort,
-          cameraDisabled: this.#cameraDisabledPort,
-          cameraConnecting: this.#connectingPort,
-        })
-        socket.emit('ice', this.#app.iceService.asRTCIceServers)
-        socket.emit('hosts', [
-          '127.0.0.1',
-          '::1',
-          ...this.#app.natService.lanIps,
-          this.#app.natService.publicIp,
-        ])
-      }
+      socket.emit('ice', this.#app.iceService.asRTCIceServers)
+      socket.emit('hosts', [
+        '127.0.0.1',
+        '::1',
+        ...this.#app.natService.lanIps,
+        this.#app.natService.publicIp,
+      ])
     })
     this.#internalApiServer.listen(this.#internalApiPort)
     this.#logger.info(`Streamer Service API listening on port ${this.#internalApiPort}`)
